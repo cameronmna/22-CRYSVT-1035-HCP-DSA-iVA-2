8d45ea8b388d31f7b17ac2e6c5c44083aecc73f7
cameron cashwell <cameron.cashwell@mna.co>
2023-03-02 12:41:40 -0800

    thumbs

--- slides/shared/js/shared.js
+++ slides/shared/js/shared.js
@@ -4,6 +4,8 @@ window.addEventListener('load', function() {
   var buttons = document.querySelectorAll(".modal-button");
   var closeButtons = document.querySelectorAll(".close");
 
+  handlePageLinks();
+
   buttons.forEach(function(button, index) {
     button.addEventListener("click", function() {
       modals[index].style.display = "block";
@@ -17,4 +19,20 @@ window.addEventListener('load', function() {
       });
     });
   });
+
+  var thisHref, slideName;
+  function handlePageLinks(){
+      document.querySelectorAll('.page-link').forEach(item => {
+          item.addEventListener('click', event => {
+              if ('ontouchstart' in document.documentElement){
+                  event.preventDefault();
+                  thisHref = item.getAttribute('href').replace('../','');
+                  slideName = thisHref + '.zip'
+                  com.veeva.clm.gotoSlide(slideName,"");
+                  return;
+              }
+          });
+      });
+  }
+
 });
